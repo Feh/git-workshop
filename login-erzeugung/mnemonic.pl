bdb9969805b4fe81c245ecd5da05fc01cabb451c
Julius Plenz <feh@mali>
2010-02-24 04:04:16 +0100

    Perl abgekuerzt

--- login-erzeugung/mnemonic.pl
+++ login-erzeugung/mnemonic.pl
@@ -8,9 +8,4 @@ my @vow = qw(a e i o u);
 my @con = qw(b c d f g h j k l m n p r s t v w z);
 
 my $c = 5;
-
-while($c > 0) {
-    print $con[int(rand(18))];
-    print $vow[int(rand(5))];
-    $c--;
-}
+print $con[int(rand(18))] . $vow[int(rand(5))] while($c--);
