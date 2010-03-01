7f743196dd9f32a9535ae6f6e7471a7507da357e
Julius Plenz <feh@mali>
2010-03-01 01:11:39 +0100

    create-user.sh nur als root ausfuehrbar; chpasswd verwenden

--- login-erzeugung/create-user.sh
+++ login-erzeugung/create-user.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+if [[ $EUID -ne 0 ]]; then
+    echo "Must be run with root privileges!" >&2
+    exit 1
+fi
+
 for i in `seq -w 1 20`; do
     user="tn$i"
     pass="`./mnemonic.pl`"
-    echo adduser --shell /usr/bin/git-shell $user
-    echo usermod -p $pass $user # hier muss man mcrypt verwenden
+    adduser --shell /usr/bin/git-shell $user
+    echo "$user:$pass" | chpasswd
 done
