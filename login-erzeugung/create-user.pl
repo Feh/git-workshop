6e47c233fe8bbcfb198dd6a7bdca9c4393be1773
Julius Plenz <feh@deepthought>
2010-03-13 12:08:53 +0100

    Passwörter kürzer gemacht (vier Zeichen)

--- login-erzeugung/create-user.pl
+++ login-erzeugung/create-user.pl
@@ -7,7 +7,7 @@ use warnings;
 sub mnemonic {
     my @vow = qw(a e i o u);
     my @con = qw(b c d f g h j k l m n p r s t v w z);
-    my $c = 5;
+    my $c = 2;
     my $pass = "";
     $pass .= $con[int(rand(18))] . $vow[int(rand(5))] while($c--);
     return $pass;
